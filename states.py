96778d3bd8c73e8aa506d20e182e3a99f1859612
Fererra <ivanglebov2007@gmail.com>
2026-09-05 18:53:13 +0500

    Update states.py

--- states.py
+++ states.py
@@ -1,16 +1,14 @@
 from telegram.ext import ConversationHandler
 
-# ============ СОСТОЯНИЯ ДЛЯ СОЗДАНИЯ ЗАПРОСА ============
 (
-    SCREENSHOT,      # 0
-    MEDIA_LINK,      # 1
-    CHANNEL_NAME,    # 2
-    SUBSCRIBERS,     # 3
-    CONTACT_LINK,    # 4
-    CONDITIONS       # 5
+    SCREENSHOT,
+    MEDIA_LINK,
+    CHANNEL_NAME,
+    SUBSCRIBERS,
+    CONTACT_LINK,
+    CONDITIONS
 ) = range(6)
 
-# ============ СОСТОЯНИЯ ДЛЯ АДМИН-ДЕЙСТВИЙ ============
 WAITING_USERNAME = 10
 WAITING_USER_ID = 11
 WAITING_KEYWORD = 12
